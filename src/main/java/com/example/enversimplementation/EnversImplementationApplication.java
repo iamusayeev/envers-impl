fb66053bdf899221135a4b75331e6b8cab445758
iamusayev <musayevanar1337@gmail.com>
2022-07-22 17:40:19 +0400

    added envers impl

---
+++ src/main/java/com/example/enversimplementation/EnversImplementationApplication.java
@@ -0,0 +1,16 @@
+package com.example.enversimplementation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.envers.repository.support.EnversRevisionRepositoryFactoryBean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories(repositoryFactoryBeanClass = EnversRevisionRepositoryFactoryBean.class)
+public class EnversImplementationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnversImplementationApplication.class, args);
+    }
+
+}
